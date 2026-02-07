7fbcf5295123e630acbe4b2818b076978db67c34
Artstrel <82947814+Artstrel@users.noreply.github.com>
2026-02-07 11:26:25 +0400

    Change default port from 10000 to 8080

--- keep_alive.py
+++ keep_alive.py
@@ -10,7 +10,7 @@ def home():
 
 def run():
     # Render передает порт в переменную PORT. Если её нет, берем 10000 (стандарт Render)
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
 
 def start_server():
