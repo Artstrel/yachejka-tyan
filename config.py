69d907b82326d4987edc7778f644ea77535cc6de
Artstrel <82947814+Artstrel@users.noreply.github.com>
2026-02-10 10:34:06 +0400

    Update config.py

--- config.py
+++ config.py
@@ -10,10 +10,5 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 DB_NAME = os.getenv("DB_NAME", "yachejka_bot")
 ADMIN_IDS = [int(id) for id in os.getenv("ADMIN_IDS", "").split(",") if id]
 
-# === ID ВЕТКИ С АНОНСАМИ ===
-# Замените 0 на реальный ID, который вы узнали в логах!
-# Например: ANNOUNCEMENT_THREAD_ID = 2
-ANNOUNCEMENT_THREAD_ID = 349332
-
 if not TELEGRAM_TOKEN or not OPENROUTER_API_KEY:
     raise ValueError("❌ Не заданы TELEGRAM_TOKEN или OPENROUTER_API_KEY")
